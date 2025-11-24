d978c88617299be659b43c6f2453477a695c0926
RapeephatPp <rapeephat.prat@bumail.net>
2025-11-25 06:02:11 +0700

    Add lighting and decor in BaseStage
    Introduced new 2D and global lights to SampleScene for improved visual effects. Added Base_Decor sprite and updated BaseScene and SampleScene with new materials, sprites, and transforms. Modified BulletRange card effect value and text. Updated Enemy_Walker prefab with new sprite, collider size, and added Animator component.

---
+++ MePpppppppp/Assets/Scripts/Penduling.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Penduling : MonoBehaviour
+{
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    public float direction = 1;
+    
+    private Quaternion startPos;
+
+    void Start()
+    {
+        startPos = transform.rotation;
+    }
+
+    void Update()
+    {
+        Quaternion a = startPos;
+        a.x += direction * (delta * Mathf.Sin(Time.time * speed));
+        transform.rotation = a;
+    }
+}
